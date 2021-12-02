56188f762e78763e35e546b1271dd03b86517ad8
abc <abc@MacBook-Pro.local>
2021-12-02 15:11:02 -0600

    remove hero from home

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,29 +14,10 @@ import Devs from '../components/Cards/assets/Devs.svg';
 
 const Home = () => {
     return (
-        <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
-            <div className="row px-3" style={{textAlign: 'left', margin: '0rem 0'}}>
-                <div className="col-lg-8" style={{padding: '4rem 0'}}>
-                    <h1 style={{fontSize: "2.5rem"}}>Welcome to Nomad Academy</h1>
-                    <p className="lead" style={{fontSize:"2rem"}}>A space to learn Programming.</p>
-                    <p className="lead">
-                    <a href="#" class="btn btn-lg btn-outline-light fw-bold">Learn more</a>
-                    </p>
-                </div>
-                <div className="col-lg-4" style={{padding: '0rem 0'}}>
-                    <img src={Programmer} style={{height: '45%',width: 'auto', objectFit: 'contain'}}/>
-                </div>
-            </div>
-            <div className="row px-3" style={{textAlign: 'left', margin: '5rem 0'}}>
-                <div className="col-lg-4" style={{padding: '2rem 0'}}>
-                    <img src={Devs} style={{width: '100%', objectFit: 'contain'}}/>
-                </div> 
-                <div className="col-lg-8" style={{padding: '3rem 0'}}>
-                    <p className="lead" style={{fontSize:"2rem"}}>Here's what you can do.</p>
-                    <p>Expedita voluptas omnis cupiditate totam eveniet nobis sint iste. Dolores est repellat corrupti reprehenderit.</p>
-                </div>
-            </div>
+        <div>
+            <h1>Hello World</h1>
         </div>
+        
 
     );
 };
